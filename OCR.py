eef1ed53a79b7d827022cdab175c6d843676124b
Jiali03713 <shi524@purdue.edu>
2024-09-23 13:24:22 -0400

    update OCR

--- OCR.py
+++ OCR.py
@@ -1,11 +1,27 @@
-import easyocr
 # Check this out when you find time
 # https://stackoverflow.com/questions/23595497/best-way-to-allow-users-to-upload-and-download-files-across-multiple-platforms
 
+# import easyocr
+import PyPDF2
+# reader = easyocr.Reader(['ch_tra', 'en'])
 
-reader = easyocr.Reader(['ch_tra', 'en'])
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
-print("Good Bye!")
+# Function to extract text from a PDF file
+def extract_text_from_pdf(pdf_path):
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text()
+    return text
+
 
 if __name__ == "__main__":
-    print("Hello!")
+    # Open a file dialog to select the PDF file
+    Tk().withdraw()  # Hide the main Tkinter window
+    pdf_file = askopenfilename(title="Select PDF file", filetypes=[("PDF files", "*.pdf")])
+    if pdf_file:
+        text = extract_text_from_pdf(pdf_file)
+        print(text)
