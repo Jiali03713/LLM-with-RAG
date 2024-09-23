2fa430c3531d64792debec5912bb61a048ccfad1
Jiali03713 <shi524@purdue.edu>
2024-09-23 12:19:23 -0400

    add OCR.py file

---
+++ OCR.py
@@ -0,0 +1,11 @@
+import easyocr
+# Check this out when you find time
+# https://stackoverflow.com/questions/23595497/best-way-to-allow-users-to-upload-and-download-files-across-multiple-platforms
+
+
+reader = easyocr.Reader(['ch_tra', 'en'])
+
+print("Good Bye!")
+
+if __name__ == "__main__":
+    print("Hello!")
