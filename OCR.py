00c63cdf0ae8e338971014f5c18d9e520fe6baea
Jiali03713 <shi524@purdue.edu>
2024-09-23 20:41:22 -0400

    modified OCR to handle errors and utf-8 format

--- OCR.py
+++ OCR.py
@@ -1,51 +1,87 @@
-# Check this out when you find time
-# https://stackoverflow.com/questions/23595497/best-way-to-allow-users-to-upload-and-download-files-across-multiple-platforms
-
-# import easyocr
 import PyPDF2
-# reader = easyocr.Reader(['ch_tra', 'en'])
 import os
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
-
+from contextlib import contextmanager
+import pdfplumber
 
 def save_text_to_file(text, file_name):
-    with open(file_name+".txt", 'w') as file:
+    with open(file_name + ".txt", 'w', encoding='utf-8') as file:
         file.write(text)
 
-# Function to extract text from a PDF file
-def extract_text_from_pdf(pdf_path, ):
 
-    # store output into a different folder
+# Extract text with PyPDF2
+# Not used, using pdfPlumber
+def extract_text_pypdf2(pdf_path):
     output_folder = "pdfReader_output"
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    # Read the content of a pdf
-    with open(pdf_path, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
-        text = ""
-        for page in reader.pages:
-            text += page.extract_text()
-    
-    # save to dict
-    file_name = os.path.splitext(os.path.basename(pdf_path))[0]
-    save_text_to_file(text, output_folder+ "/"+file_name)
+    try:
+        with open(pdf_path, 'rb') as file:
+            reader = PyPDF2.PdfReader(file)
+            text = ""
+            for page in reader.pages:
+                text += page.extract_text()
 
+    except FileNotFoundError as e:
+        print(f"File not found: {e}")
+        return None
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+    file_name = os.path.splitext(os.path.basename(pdf_path))[0]
+    save_text_to_file(text, os.path.join(output_folder, file_name))
+    
+    print(f"Text successfully extracted and saved to {os.path.join(output_folder, file_name)}.txt")
+    return text
+
+# Extract text with pdfPlumber
+def extract_text_pdfplumber(pdf_path):
+    output_folder = "pdfReader_output"
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    try:
+        with pdfplumber.open(pdf_path) as pdf:
+            text = ''
+            for page in pdf.pages:
+                text += page.extract_text()
+
+    except FileNotFoundError as e:
+        print(f"File not found: {e}")
+        return None
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+    file_name = os.path.splitext(os.path.basename(pdf_path))[0]
+    save_text_to_file(text, os.path.join(output_folder, file_name))
+    
+    print(f"Text successfully extracted and saved to {os.path.join(output_folder, file_name)}.txt")
     return text
 
 
+@contextmanager
+def use_tkinter():
+    root = Tk()
+    root.withdraw()
+    try:
+        yield root
+    finally:
+        root.quit()
 
 def select_pdf():
-    Tk().withdraw()  # Hide the main Tkinter window
+    with use_tkinter():
+        pdf_file = askopenfilename(title="Select PDF file", filetypes=[("PDF files", "*.pdf")])
 
-    pdf_file = askopenfilename(title="Select PDF file", filetypes=[("PDF files", "*.pdf")])
-
-    if pdf_file:
-        text = extract_text_from_pdf(pdf_file)
-        print(text)
-
+        if pdf_file:
+            extract_text_pdfplumber(pdf_file)
+        else:
+            print("No file selected or invalid file.")
 
 if __name__ == "__main__":
-    # Open a file dialog to select the PDF file
     select_pdf()
