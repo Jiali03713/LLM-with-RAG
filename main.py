fcbe43052c3e0cf64dd9752b30b7327cfd6fb422
Jiali03713 <shi524@purdue.edu>
2024-10-01 15:00:40 -0400

    Modifty main.py

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import pdf_to_txt
 
 
 if __name__ == "__main__":
+    # if user want to input new pdf
     txt_path = pdf_to_txt.run()
-    question = input("Enter the question: ")
-    milvus_rag.call_func(txt_path, question)
+    milvus_rag.run(txt_path)
+
+    #TODO: user what to query existing collection
+
+    # milvus_client = input("Enter the milvus client name: ")
+    # collection_name = input("Enter the milvus collection name: ")
 
 
