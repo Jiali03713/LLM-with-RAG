92e3ff2ac570127d6cb08c6f53072e6887f7b5fa
Jiali Shi <jiali@Pancake.Pancake>
2024-10-01 14:18:58 -0400

    Modify pdf_to_txt.py

--- pdf_to_txt.py
+++ pdf_to_txt.py
@@ -13,22 +13,12 @@ def save_text_to_file(texts, file_name):
     with open(file_name + ".txt", 'a', encoding='utf-8') as file:
         for text in texts:
             file.write(text + "\n")
-    
-    # Check if the file is a PDF by checking its MIME type
-    mime_type, _ = mimetypes.guess_type(file_path)
-    
-    if mime_type == 'application/pdf':
-        print("The file is a valid PDF.")
-        return True
-    else:
-        print("Error: The file is not a PDF.")
-        return False
 
 
 # Extract text with pdfPlumber
 def extract_text_pdfplumber(pdf_path):
     output_folder = "pdfReader_output"
-    
+
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
@@ -44,7 +34,7 @@ def extract_text_pdfplumber(pdf_path):
     except FileNotFoundError as e:
         print(f"File not found: {e}")
         return None
-    
+
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
@@ -52,7 +42,6 @@ def extract_text_pdfplumber(pdf_path):
     file_name = os.path.splitext(os.path.basename(pdf_path))[0]
     text_list = text.split(".")
     text_list = [element for element in text_list if element and isinstance(element, str) and len(element) > 10]
-
 
     save_text_to_file(text_list, os.path.join(output_folder, file_name))
     print(f"Text successfully extracted and saved to {os.path.join(output_folder, file_name)}.txt")
@@ -79,11 +68,10 @@ def select_pdf():
             print("No file selected or invalid file.")
 
 if __name__ == "__main__":
-    import sys; 
-    print(sys.stdin.isatty())
-    if sys.stdin.isatty():
+    import sys;
+
+    if os.getenv("$DISPLAY") is not None:
         select_pdf()
     else:
         file_path = input("Enter the path of your pdf file: ")
         extract_text_pdfplumber(file_path)
-
