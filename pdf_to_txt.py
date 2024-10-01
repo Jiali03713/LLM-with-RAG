425ff97302fb93f757171edbc250ddb143946015
Jiali03713 <shi524@purdue.edu>
2024-10-01 13:34:38 -0400

    Modify pdf_to_txt.py to detect GUI

--- pdf_to_txt.py
+++ pdf_to_txt.py
@@ -4,6 +4,7 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 from contextlib import contextmanager
 import pdfplumber
+import mimetypes
 
 def save_text_to_file(texts, file_name):
     if os.path.exists(file_name + ".txt"):
@@ -12,38 +13,22 @@ def save_text_to_file(texts, file_name):
     with open(file_name + ".txt", 'a', encoding='utf-8') as file:
         for text in texts:
             file.write(text + "\n")
-
-# Extract text with PyPDF2
-# Not used, using pdfPlumber
-# def extract_text_pypdf2(pdf_path):
-    # output_folder = "pdfReader_output"
-    # if not os.path.exists(output_folder):
-    #     os.makedirs(output_folder)
-
-    # try:
-    #     with open(pdf_path, 'rb') as file:
-    #         reader = PyPDF2.PdfReader(file)
-    #         text = ""
-    #         for page in reader.pages:
-    #             text += page.extract_text()
-
-    # except FileNotFoundError as e:
-    #     print(f"File not found: {e}")
-    #     return None
     
-    # except Exception as e:
-    #     print(f"An error occurred: {e}")
-    #     return None
-
-    # file_name = os.path.splitext(os.path.basename(pdf_path))[0]
-    # save_text_to_file(text, os.path.join(output_folder, file_name))
+    # Check if the file is a PDF by checking its MIME type
+    mime_type, _ = mimetypes.guess_type(file_path)
     
-    # print(f"Text successfully extracted and saved to {os.path.join(output_folder, file_name)}.txt")
-    # return text
+    if mime_type == 'application/pdf':
+        print("The file is a valid PDF.")
+        return True
+    else:
+        print("Error: The file is not a PDF.")
+        return False
+
 
 # Extract text with pdfPlumber
 def extract_text_pdfplumber(pdf_path):
     output_folder = "pdfReader_output"
+    
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
@@ -67,15 +52,11 @@ def extract_text_pdfplumber(pdf_path):
     file_name = os.path.splitext(os.path.basename(pdf_path))[0]
     text_list = text.split(".")
     text_list = [element for element in text_list if element and isinstance(element, str) and len(element) > 10]
-    
-    # for text in text_list:
-    #     print("+++++" + text + "+++++++")
-    #     print('')
+
 
     save_text_to_file(text_list, os.path.join(output_folder, file_name))
-    
     print(f"Text successfully extracted and saved to {os.path.join(output_folder, file_name)}.txt")
-    
+
     return text
 
 
@@ -98,5 +79,11 @@ def select_pdf():
             print("No file selected or invalid file.")
 
 if __name__ == "__main__":
-    select_pdf()
+    import sys; 
+    print(sys.stdin.isatty())
+    if sys.stdin.isatty():
+        select_pdf()
+    else:
+        file_path = input("Enter the path of your pdf file: ")
+        extract_text_pdfplumber(file_path)
 
