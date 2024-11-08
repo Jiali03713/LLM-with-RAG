d3cdd585af29220e02c8e7f9209305b311a613d3
Jiali03713 <shi524@purdue.edu>
2024-11-08 14:23:34 -0500

    add txt option

--- main.py
+++ main.py
@@ -7,16 +7,25 @@ if __name__ == "__main__":
         NVIDIA_API_KEY = input("Add your NVIDIA key here: ")
         os.environ['NVIDIA_API_KEY'] = NVIDIA_API_KEY
 
+
     # if user want to input new pdf
     new_pdf = input("Do you want to input a new pdf[Y/N]: ")
     if new_pdf.lower()== "y":
-        txt_path = pdf_to_txt.run()
-        milvus_rag.run_new(txt_path)
+        type = input("Pdf or txt?: ")
+        if type == "pdf":
+            txt_path = pdf_to_txt.run()
+            milvus_rag.run_new(txt_path)
+        if type == "txt":
+            txt_path = input("txtpath?: ")
+            milvus_rag.run_new(txt_path)
+
+
 
     elif new_pdf.lower()=="n":
         collection_name = input("Enter your collection name: ")
         milvus_rag.run_old(collection_name)
     
+
     else:
         # TODO implement resursive call
         print("Invalid input. Better luck next time. Bye.")
